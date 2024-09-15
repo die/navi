52abd0b8ab9fb63fdd678720e09f45fce228cc3d
die <48879283+die@users.noreply.github.com>
2024-09-15 14:38:05 +0000

    Remove eslint prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
     {
@@ -60,5 +59,4 @@ export default [
         },
     },
     pluginJs.configs.recommended,
-    eslintConfigPrettier,
 ];
