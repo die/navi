604092a8ad4123114546a9d6862e967ea00ff342
die <48879283+die@users.noreply.github.com>
2024-09-15 02:30:44 +0000

    Add streaming presence

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 const getDisTube = require('../lib/distube');
 
 module.exports = {
@@ -7,7 +7,15 @@ module.exports = {
 	startTime: Date.now(),
 	async execute(client) {
         client.application_emojis = await client.application.emojis.fetch();
+
         getDisTube(client);
+
+        client.user.setActivity({
+            name: 'the wired',
+            type: ActivityType.Streaming,
+            url: 'https://www.youtube.com/watch?v=Hi2OSnyERq0'
+        });
+
         console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
